a53f8e38b20985642a904263cbf56a794a4cba12
JoeDimmick <JoeWDimmick@gmail.com>
2020-12-02 17:34:58 -0700

    deployment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './src/stylesheets/main.scss';
-import App from './src/App';
-import * as serviceWorker from './src/serviceWorker';
+import './stylesheets/main.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
