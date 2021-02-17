f6fe011895bf149117ff65990811aa1423947f2d
BradyBlackley <bradyblackley@mail.weber.edu>
2021-02-16 21:38:50 -0700

    added quiz navigation and basic quiz functionality

--- src/components/SelectedQuiz.js
+++ src/components/SelectedQuiz.js
@@ -3,47 +3,14 @@ import axios from 'axios';
 import { Row } from 'reactstrap';
 
 function QuizQuestion({ questionList }) {
-	return (
-		<div className='SelectedQuiz'>
-			{/* {showScore && (<div className='score-section'>You scored {score} out of {}</div>)} */}
-			{/* {(!showScore && questionList.length > 0) ? questionSection : null} */}
-			{(questionList.length > 0) ?
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<Row>Question: {questionList[0].question}</Row>
-							<Row>Grade Level: {questionList[0].gradeLevel}</Row>
-						</div>
-						<br />
-						<div>
-							<Row>Question: {questionList[1].question}</Row>
-							<Row>Grade Level: {questionList[1].gradeLevel}</Row>
-						</div>
-						<div className='question-text'>{ }</div>
-					</div>
-					<div className='answer-section'>
-						{/* {questions[currentQuestion].answerOptions.map((answerOption)=> 
-					<button onClick={()=> handleAnswerButtonClick(answerOption.isCorrect)}>{answerOption.answerText}</button>)} */}
-					</div>
-				</> : null}
-		</div>
-	);
-}
-
-function SelectedQuiz() {
 
 	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [questionList, setQuestionList] = useState([]);
 
 	const [showScore, setShowScore] = useState(false);
 
 	const [score, setScore] = useState(0);
 
-	const handleAnswerButtonClick = (isCorrect) => {
-
-		if (isCorrect === true) {
-			setScore(score + 1);
-		}
+	const nextQuestion = () => {
 
 		const nextQuestion = currentQuestion + 1;
 		if (nextQuestion < questionList.length) {
@@ -53,11 +20,48 @@ function SelectedQuiz() {
 		}
 	};
 
+	const previousQuestion = () => {
+
+		const prevQuestion = currentQuestion - 1;
+		if (prevQuestion >= 0) {
+			setCurrentQuestion(prevQuestion);
+		} 
+	};
+
+	return (
+		<div className='SelectedQuiz'>
+			{/* {showScore && (<div className='score-section'>You scored {score} out of {}</div>)} */}
+			{/* {(!showScore && questionList.length > 0) ? questionSection : null} */}
+			{(questionList.length > 0) ?
+				<>
+					<div className='question-section'>
+						<div className='question-text'>
+								<Row>Question {currentQuestion + 1}: {questionList[currentQuestion].question}</Row>
+						</div>
+						<div className='answer-section'>
+							<Row><button>{questionList[currentQuestion].answer}</button></Row>
+							<Row><button>{questionList[currentQuestion].incorrectAnswer1}</button></Row>
+							<Row><button>{questionList[currentQuestion].incorrectAnswer2}</button></Row>
+							<Row><button>{questionList[currentQuestion].incorrectAnswer3}</button></Row>
+						</div>
+						<div className='question-navigation'>
+							<Row><button onClick={()=> previousQuestion()}>Previous</button><button onClick={()=> nextQuestion()}>Next</button></Row>
+						</div>
+					</div>
+				</> : null}
+		</div>
+	);
+}
+
+function SelectedQuiz() {
+
+	const [questionList, setQuestionList] = useState([]);
+
 	// http://localhost:8080/assessment/questions 
 	useEffect(() => {
 		async function fetchData() {
 			const response = await axios.post(`http://localhost:8080/assessment/questions`, {
-				"assessmentID": 2,
+				"assessmentID": 1,
 			})
 				.then(response => {
 					setQuestionList(response.data)
