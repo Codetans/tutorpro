09fa12dab8c596ee1888a75fa442bad90fe3a498
David Johansen <davidjohansen1@mail.weber.edu>
2021-02-23 20:08:24 -0700

    Recording which answer was selected and hiding the next and previous buttons

--- src/components/SelectedQuiz.js
+++ src/components/SelectedQuiz.js
@@ -2,31 +2,48 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Row } from 'reactstrap';
 
+var answers = [];
+
 function QuizQuestion({ questionList }) {
 
-	const [currentQuestion, setCurrentQuestion] = useState(0);
+	var [currentQuestion, setCurrentQuestion] = useState(0);
 
 	const [showScore, setShowScore] = useState(false);
 
 	const [score, setScore] = useState(0);
 
-	const nextQuestion = () => {
+	console.log(questionList);
 
-		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questionList.length) {
-			setCurrentQuestion(nextQuestion);
+
+	const nextQuestion = () => {
+		console.log("The answer held in answer " + 0 + " is " + answers[0])
+		console.log("The answer held in answer " + 1 + " is " + answers[1])
+		console.log("The answer held in answer " + 2 + " is " + answers[2])
+		currentQuestion++;
+		if (currentQuestion <= questionList.length) {
+			setCurrentQuestion(currentQuestion);
 		} else {
+			currentQuestion--;
 			setShowScore(true);
 		}
 	};
 
 	const previousQuestion = () => {
-
-		const prevQuestion = currentQuestion - 1;
-		if (prevQuestion >= 0) {
-			setCurrentQuestion(prevQuestion);
+		console.log("The answer held in answer " + 0 + " is " + answers[0])
+		console.log("The answer held in answer " + 1 + " is " + answers[1])
+		console.log("The answer held in answer " + 2 + " is " + answers[2])
+		currentQuestion--;
+		if (currentQuestion >= 0) {
+			setCurrentQuestion(currentQuestion);
 		} 
 	};
+
+	function handleInput(answer, questionNumber) {
+		if(answer) {
+			console.log("Updating the stored question " + questionNumber + " to be " + answer.target.value)
+			answers[questionNumber] = answer.target.value
+		}
+	}
 
 	return (
 		<div className='SelectedQuiz'>
@@ -39,13 +56,14 @@ function QuizQuestion({ questionList }) {
 								<Row>Question {currentQuestion + 1}: {questionList[currentQuestion].question}</Row>
 						</div>
 						<div className='answer-section'>
-							<Row><button>{questionList[currentQuestion].answer}</button></Row>
-							<Row><button>{questionList[currentQuestion].incorrectAnswer1}</button></Row>
-							<Row><button>{questionList[currentQuestion].incorrectAnswer2}</button></Row>
-							<Row><button>{questionList[currentQuestion].incorrectAnswer3}</button></Row>
+							<Row><button onClick={e => handleInput(e, currentQuestion)} value={questionList[currentQuestion].answer}>{questionList[currentQuestion].answer}</button></Row>
+							<Row><button onClick={e => handleInput(e, currentQuestion)} value={questionList[currentQuestion].incorrectAnswer1}>{questionList[currentQuestion].incorrectAnswer1}</button></Row>
+							<Row><button onClick={e => handleInput(e, currentQuestion)} value={questionList[currentQuestion].incorrectAnswer2}>{questionList[currentQuestion].incorrectAnswer2}</button></Row>
+							<Row><button onClick={e => handleInput(e, currentQuestion)} value={questionList[currentQuestion].incorrectAnswer3}>{questionList[currentQuestion].incorrectAnswer3}</button></Row>
 						</div>
 						<div className='question-navigation'>
-							<Row><button onClick={()=> previousQuestion()}>Previous</button><button onClick={()=> nextQuestion()}>Next</button></Row>
+							{/* <Row><button onClick={()=> previousQuestion()}>Previous</button><button onClick={()=> nextQuestion()}>Next</button></Row> */}
+							<Row>{(currentQuestion > 0) ? (<button onClick={()=> previousQuestion()}>Previous</button>) : (null)}{(currentQuestion < questionList.length-1) ? (<button onClick={()=> nextQuestion()}>Next</button>) : (null)}</Row>
 						</div>
 					</div>
 				</> : null}
@@ -61,7 +79,7 @@ function SelectedQuiz() {
 	useEffect(() => {
 		async function fetchData() {
 			const response = await axios.post(`http://localhost:8080/assessment/questions`, {
-				"assessmentID": 1,
+				"assessmentID": 2,
 			})
 				.then(response => {
 					setQuestionList(response.data)
