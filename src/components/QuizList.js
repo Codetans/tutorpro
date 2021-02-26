77a15926270e74018527d7d1ef660ca9e151bda4
BradyBlackley <bradyblackley@mail.weber.edu>
2021-02-25 20:14:45 -0700

    renamed files split selectedquiz into two separate components

---
+++ src/components/QuizList.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Jumbotron, Container, Card, CardImg, CardTitle, CardText, Button, Row, Col } from 'reactstrap';
+import img from './assets/calculator.jpg'
+import img2 from './assets/science.jpg'
+import img3 from './assets/english.jpg'
+
+  // we need to abstract this class. Quiz titles and other info from DB.
+
+const QuizList = (props) => {
+
+    return (
+        <div>
+            <Jumbotron className="jumbotron">
+            <Row>
+            <Col sm="4">
+                <Card>
+                    <br />
+                    <CardTitle tag="h5"><b>Math Quiz - Multiplication</b></CardTitle>
+                    <CardImg top width="100%" src={img} alt="Math Calculator" />
+                    <br />
+                    <CardText>This quiz will assess your understanding of multiplication factors </CardText>
+                    <Button color="info" tag="a" href="#" onClick={() => {props.changeMode("selectedQuiz")}}>Take this quiz</Button>
+                </Card>
+            </Col>
+            <Col sm="4">
+                <Card>
+                    <br />
+                    <CardTitle tag="h5"><b>Science Quiz</b></CardTitle>
+                    <CardImg top width="100%" src={img2} alt="Science" />                    
+                    <br />
+                    <CardText>Take this quiz to gain a better understand of Biology</CardText>
+                    <Button color="info" tag="a" href="#" onClick={() => {props.changeMode("selectedQuiz")}}>Take this quiz</Button>
+                </Card>
+            </Col>
+            <Col sm="4">
+                <Card>
+                    <br />
+                    <CardTitle tag="h5">Language arts quiz</CardTitle>
+                    <CardImg top width="100%" src={img3} alt="English" />
+                    <br />
+                    <CardText>This quiz is intended to assess your reading comprehension skils</CardText>
+                    <Button color="info" tag="a" href="#" onClick={() => {props.changeMode("selectedQuiz")}}>Take this quiz</Button>
+                </Card>
+            </Col>
+            </Row>
+            </Jumbotron>
+        </div>
+    );
+}
+
+export default QuizList;
---
+++ src/components/QuizQuestions.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { Row } from 'reactstrap';
+
+var answers = [];
+
+function QuizQuestions({ questionList }) {
+
+	var [currentQuestion, setCurrentQuestion] = useState(0);
+
+	const [showScore, setShowScore] = useState(false);
+
+	const [score, setScore] = useState(0);
+
+	console.log(questionList);
+
+	const nextQuestion = () => {
+		console.log("The answer held in answer " + 0 + " is " + answers[0])
+		console.log("The answer held in answer " + 1 + " is " + answers[1])
+		console.log("The answer held in answer " + 2 + " is " + answers[2])
+		currentQuestion++;
+		if (currentQuestion <= questionList.length) {
+			setCurrentQuestion(currentQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+
+	const previousQuestion = () => {
+		console.log("The answer held in answer " + 0 + " is " + answers[0])
+		console.log("The answer held in answer " + 1 + " is " + answers[1])
+		console.log("The answer held in answer " + 2 + " is " + answers[2])
+		currentQuestion--;
+		if (currentQuestion >= 0) {
+			setCurrentQuestion(currentQuestion);
+		} 
+	};
+
+	function handleInput(answer, questionNumber) {
+		if(answer) {
+			console.log("Updating the stored question " + questionNumber + " to be " + answer.target.value)
+			answers[questionNumber] = answer.target.value
+		}
+	}
+
+	return (
+		<div className='SelectedQuiz'>
+			{/* {showScore && (<div className='score-section'>You scored {score} out of {}</div>)} */}
+			{/* {(!showScore && questionList.length > 0) ? questionSection : null} */}
+			{(questionList.length > 0) ?
+				<>
+					<div className='question-section'>
+						<div className='question-text'>
+								<Row>Question {currentQuestion + 1}: {questionList[currentQuestion].question}</Row>
+						</div>
+						<div className='answer-section'>
+							<Row><button onClick={e => handleInput(e, currentQuestion)} value={questionList[currentQuestion].answer}>{questionList[currentQuestion].answer}</button></Row>
+							<Row><button onClick={e => handleInput(e, currentQuestion)} value={questionList[currentQuestion].incorrectAnswer1}>{questionList[currentQuestion].incorrectAnswer1}</button></Row>
+							<Row><button onClick={e => handleInput(e, currentQuestion)} value={questionList[currentQuestion].incorrectAnswer2}>{questionList[currentQuestion].incorrectAnswer2}</button></Row>
+							<Row><button onClick={e => handleInput(e, currentQuestion)} value={questionList[currentQuestion].incorrectAnswer3}>{questionList[currentQuestion].incorrectAnswer3}</button></Row>
+						</div>
+						<div className='question-navigation'>
+							{/* <Row><button onClick={()=> previousQuestion()}>Previous</button><button onClick={()=> nextQuestion()}>Next</button></Row> */}
+							<Row>{(currentQuestion > 0) ? (<button onClick={()=> previousQuestion()}>Previous</button>) : (null)}{(currentQuestion < questionList.length-1) ? (<button onClick={()=> nextQuestion()}>Next</button>) : (null)}</Row>
+						</div>
+					</div>
+				</> : null}
+		</div>
+	);
+}
+
+export default QuizQuestions;
